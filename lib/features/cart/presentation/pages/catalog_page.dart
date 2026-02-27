be3addabc21dd2d6d41335e492233d58d7f54941
hartamatamatama <katonojutsu340@gmail.com>
2026-02-27 15:42:50 +0700

    Add class CatalogPage

--- lib/features/cart/presentation/pages/catalog_page.dart
+++ lib/features/cart/presentation/pages/catalog_page.dart
@@ -2,3 +2,6 @@ import 'package:flutter/material.dart';
 import '../../../../core/routes/app_router.dart';
 import '../../domain/entities/product.dart';
 import '../widgets/add_button_widget.dart';
+
+class CatalogPage extends StatelessWidget {
+  const CatalogPage({super.key});
