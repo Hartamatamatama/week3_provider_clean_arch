2889f7fbe3e5995d4032645ba6752c3ca73759b7
hartamatamatama <katonojutsu340@gmail.com>
2026-02-27 16:05:28 +0700

    add  Widget build

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 import 'core/routes/app_router.dart';
 import 'injection.dart';
+
 void main() => runApp(buildApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // Menggunakan konstanta route dari AppRouter di Core Layer
+      initialRoute: AppRouter.catalog,
+      routes: AppRouter.routes,
+    );
+  }
+}
