07605a22267d0d7f11dea388ac567fa9ba6cd75c
hartamatamatama <katonojutsu340@gmail.com>
2026-02-27 15:51:06 +0700

    Add class CartPage

--- lib/features/cart/presentation/pages/cart_page.dart
+++ lib/features/cart/presentation/pages/cart_page.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart_provider.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
