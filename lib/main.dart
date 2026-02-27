0a0b8ae5413ebacfd0e2443f548e69bada2b6ed7
hartamatamatama <katonojutsu340@gmail.com>
2026-02-27 16:05:12 +0700

    add void main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 import 'core/routes/app_router.dart';
 import 'injection.dart';
+void main() => runApp(buildApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
