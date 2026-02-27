84e7b6492bdb300c28387a5cbb6937acc78e08fd
hartamatamatama <katonojutsu340@gmail.com>
2026-02-27 15:43:09 +0700

    Add final List<Product> _products

--- lib/features/cart/presentation/pages/catalog_page.dart
+++ lib/features/cart/presentation/pages/catalog_page.dart
@@ -5,3 +5,8 @@ import '../widgets/add_button_widget.dart';
 
 class CatalogPage extends StatelessWidget {
   const CatalogPage({super.key});
+  static final List<Product> _products = [
+    const Product(id: '1', name: 'Nasi Goreng', price: 15000),
+    const Product(id: '2', name: 'Sate Ayam', price: 20000),
+    const Product(id: '3', name: 'Es Teh', price: 5000),
+  ];
