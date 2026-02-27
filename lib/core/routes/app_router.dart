0bd2c2c80cbe52d5d3ebdbaffbb7d88c89185482
hartamatamatama <katonojutsu340@gmail.com>
2026-02-27 15:42:29 +0700

    Add import

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// Import ini akan merah dulu, abaikan saja karena file-nya belum kita pindahkan
 import '../../features/cart/presentation/pages/catalog_page.dart';
 import '../../features/cart/presentation/pages/cart_page.dart';
 
