974ad4c15f85add344a270a5b727f14a80d09bd5
hartamatamatama <katonojutsu340@gmail.com>
2026-02-27 15:09:03 +0700

    add main

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    // Membungkus aplikasi dengan ChangeNotifierProvider
+    // agar State bisa diakses di mana saja
+    ChangeNotifierProvider(
+      create: (context) => CartModel(),
+      child: const MyApp(),
+    ),
+  );
+}
