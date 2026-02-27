d4adcbced606e5f6b27b3eb8c168d1cc0436df89
hartamatamatama <katonojutsu340@gmail.com>
2026-02-27 15:45:11 +0700

    Add class AddButtonWidget

--- lib/features/cart/presentation/widgets/add_button_widget.dart
+++ lib/features/cart/presentation/widgets/add_button_widget.dart
@@ -2,3 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../domain/entities/product.dart';
 import '../providers/cart_provider.dart';
+
+class AddButtonWidget extends StatelessWidget {
+  final Product product;
